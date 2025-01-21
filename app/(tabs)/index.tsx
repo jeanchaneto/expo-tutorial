1fd19fa513537169e1e9214df8fcea3102e93bf1
jeanchaneto <92741347+jeanchaneto@users.noreply.github.com>
2025-01-21 21:43:47 +0100

    Use an image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,19 +2,47 @@ import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 import { Image } from "expo-image";
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from "expo-image-picker";
 import Button from "@/components/Button";
+import { useState } from "react";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
       <View style={styles.footerContainer}>
-        <Button label="Choose a photo" theme="primary" />
-        <Button label="Use this photo" />
+        <Button
+          label="Choose a photo"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button
+          label="Use this photo"
+          onPress={() => alert("You pressed a button.")}
+        />
       </View>
     </View>
   );
